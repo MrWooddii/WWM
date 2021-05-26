8dbf42f8529181bad1d40ef7057b52f427275dfd
Stephan Holzapfel <stephenholzapfel@aol.com>
2021-05-26 22:03:33 +0200

    Initial commit

---
+++ src/main/Controller.java
@@ -0,0 +1,21 @@
+package main;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+
+    @FXML
+    private Button AButton;
+    @FXML
+    private Button BButton;
+    @FXML
+    private Button CButton;
+    @FXML
+    private Button DButton;
+
+    public void setQuestion() {
+
+    }
+
+}
---
+++ src/main/Question.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Question {
+
+    private String question;
+    private String a;
+    private String b;
+    private String c;
+    private String d;
+
+
+}
